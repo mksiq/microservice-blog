8b773847d2c8b71883eb15563d0b0e841424ac68
mksiq <maickelsiqueira@gmail.com>
2021-04-13 11:58:24 -0400

    added event bus data structure to keep track all the events and extra functions to deal with comment and moderation service being down

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,22 +4,27 @@ const { json } = require("express");
 
 const app = express();
 
+const events = [];
+
 app.use(json());
 
 app.post("/events", (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   axios.post("http://localhost:4000/events", event);
   axios.post("http://localhost:4001/events", event);
   axios.post("http://localhost:4002/events", event);
   axios.post("http://localhost:4003/events", event);
 
+  console.log("Received Event", req.body.type);
+
   res.send({ status: "OK" });
 });
 
-app.post("/events", (req, res) => {
-  console.log('Received Event', req.body.type);
-  res.send({});
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- moderation/index.js
+++ moderation/index.js
@@ -25,6 +25,33 @@ app.post("/events", async (req, res) => {
   res.send({});
 });
 
-app.listen(4003, () => {
+app.listen(4003, async () => {
   console.log("Moderation listening on 4003");
+
+  try {
+    const res = await axios.get("http://localhost:4005/events");
+
+    for (let event of res.data) {
+      console.log("Processing event ", event.type);
+
+      if (event.type === "CommentCreated") {
+        console.log(event)
+        const status = event.data.content.includes("orange")
+          ? "rejected"
+          : "approved";
+
+        await axios.post("http://localhost:4005/events", {
+          type: "CommentModerated",
+          data: {
+            id: event.data.id,
+            postId: event.data.postId,
+            status,
+            content: event.data.content,
+          },
+        });
+      }
+    }
+  } catch (e) {
+    console.error(e);
+  }
 });
